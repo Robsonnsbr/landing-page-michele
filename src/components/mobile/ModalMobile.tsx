51ae08188e5a26eb423937c9265e90d0ca3f52aa
robsonnsbr <robsonnsbr@gmail.com>
2024-01-25 21:14:01 -0300

    fix: modalmobile

--- src/components/mobile/ModalMobile.tsx
+++ src/components/mobile/ModalMobile.tsx
@@ -4,15 +4,14 @@ interface ModalMobileProps {
   isOpen: boolean;
 }
 const styleNavMobileDisable = `absolute translate-y-14 -translate-x-3 opacity-0 h-0 overflow-hidden`;
-
-const styleNavMobileActive = `absolute translate-y-14 -translate-x-5 bg-primaryColor bg-opacity-50 z-1 
-        `;
+const styleNavMobileActive = `absolute translate-y-14 -translate-x-4 bg-primaryColor bg-opacity-50 opacity-100`;
 
 export const ModalMobile = ({ isOpen }: ModalMobileProps) => {
   return (
     <nav
-      className={` top-0 shadow-md transition-transform duration-1000
-      ease-in-out ${isOpen ? styleNavMobileActive : styleNavMobileDisable}`}
+      className={`top-0 shadow-md transition-all duration-500 ease-in-out ${
+        isOpen ? styleNavMobileActive : styleNavMobileDisable
+      }`}
     >
       <ul className="flex-col sm:grid grid-cols-7 w-full text-center border-2rounded-sm border-secondaryActiveColor">
         <li className="p-1">
